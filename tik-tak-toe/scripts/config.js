82c0ff02b7141c8550354b8d1f23f289340611b0
Anurag sharma <115712963+wraith756@users.noreply.github.com>
2024-04-07 14:26:02 +0530

    TIK TAC TOK GAME UPLOAD

---
+++ tik-tak-toe/scripts/config.js
@@ -0,0 +1,33 @@
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid; // +'1' => 1
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = '';
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim(); // '     Akku    ' -> 'Akku'
+
+    if (!enteredPlayername) { // enteredPlayername ===''
+        event.target.firstElementChild.classList.add('error');
+        errorOutputElement.textContent = 'Please enter a valid name!';
+        return;  // return stops the function further process if the if statement is executed
+    }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+    players[editedPlayer - 1].name = enteredPlayername;
+
+    closePlayerConfig();
+}
+
